e3c6c1037c935aa7f73f59961353fe18ec7d874e
Agus Wesly <agustchannel@gmail.com>
2026-01-25 09:51:07 +0700

    revert: back into specular lighting original code

--- colors_fragment.glsl
+++ colors_fragment.glsl
@@ -1,32 +1,34 @@
 #version 330 core
 
-uniform vec3 lightColor;
 uniform vec3 cubeColor;
 uniform vec3 lightPosition;
 uniform vec3 cameraPosition;
 
 in vec3 Normal;
-in vec3 fragmentPosition;
+in vec3 FragmentPosition;
 
 out vec4 FragColor;
 
+uniform vec3 lightColor;
+
 void main()
 {
-    float ambientStrength = 0.4f;
+    float ambientStrength = 0.1f;
     vec3 ambient = ambientStrength * lightColor;
 
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPosition - fragmentPosition);
+    vec3 lightDir = normalize(lightPosition - FragmentPosition);
     float diffuseStrength = max(dot(lightDir, norm), 0.0f);
     vec3 diffuse = diffuseStrength * lightColor;
 
     const float specularStrength = 0.5f;
-    vec3 cameraDir = normalize(cameraPosition - fragmentPosition);
+    vec3 cameraDir = normalize(cameraPosition - FragmentPosition);
     vec3 reflectedDir = reflect(-lightDir, norm);
-    const float SHININESS = 2.0f;
+
+    const float SHININESS = 256.0f;
     float spec = pow(max(dot(cameraDir, reflectedDir), 0.0f), SHININESS);
     vec3 specular = spec * specularStrength * lightColor;
-
     vec3 result = (diffuse + ambient + specular) * cubeColor;
+
     FragColor = vec4(result, 1.0f);
 };
--- colors_vertex.glsl
+++ colors_vertex.glsl
@@ -3,10 +3,8 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 
-
-out vec2 ourTexPos;
 out vec3 Normal;
-out vec3 fragmentPosition;
+out vec3 FragmentPosition;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -15,7 +13,8 @@ uniform mat3 normalMatrix;
 
 void main()
 {
-   fragmentPosition = vec3(model * vec4(aPos, 1.0f));
+   FragmentPosition = vec3(model * vec4(aPos, 1.0f));
    Normal = normalMatrix * aNormal;
-   gl_Position = perspective * view * vec4(fragmentPosition, 1.0f);
+
+   gl_Position = perspective * view * vec4(FragmentPosition, 1.0f);
 };
